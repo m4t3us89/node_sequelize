b06e170f8882aed368f1e9d5a4d64806e321205d
Allisson Mateus <allisson.mateus@crptecnologia.com.br>
2019-05-31 09:04:08 -0300

    sentry errors integração

--- src/server.js
+++ src/server.js
@@ -3,8 +3,29 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const path = require('path')
-
 const app = express()
+
+/* SENTRY ERROS*/
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://69f24078b5e14ae4b79810e2d5a009d5@sentry.io/1458565' });
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// The error handler must be before any other error middleware
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+    throw new Error('My first Sentry error!');
+});
+/* FIM SENTRY ERROS*/
 
 app.use(cors())
 app.use(express.json())
